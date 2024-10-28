08be44601ec21b93aaf069603ed3c27634e9d69c
Johan <johan.svensson@gtsmarketing.se>
2024-10-28 12:53:12 +0100

    ClientFix

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,10 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$env/static/private';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
-console.log('SUPABASE_URL:', SUPABASE_URL); // Debugging line
-console.log('SUPABASE_ANON_KEY:', SUPABASE_ANON_KEY); // Debugging line
-
-const supabaseUrl = SUPABASE_URL;
-const supabaseKey = SUPABASE_ANON_KEY;
+const supabaseUrl = PUBLIC_SUPABASE_URL;
+const supabaseKey = PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
